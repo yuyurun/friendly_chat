d9cb7ba36c1a95888693ae40f27b377cc6823166
yuyurun <yuyu1.run@icloud.com>
2021-05-03 14:06:26 +0900

    feat: add textfield

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,71 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      title: 'チャットだよ',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('チャットだよ'),
-        ),
-      ),
-    ),
+    FriendlyChatApp(),
   );
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'チャットだよ',
+      home: Chatscreen(),
+    );
+  }
+}
+
+class Chatscreen extends StatefulWidget {
+  @override
+  _ChatscreenState createState() => _ChatscreenState();
+}
+
+class _ChatscreenState extends State<Chatscreen> {
+  final _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('チャットだよ')), 
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return  IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child:  Row(                         
+          children: [                           
+            Flexible(                          
+              child:  TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:  InputDecoration.collapsed(
+                    hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)), 
+
+            )                                    
+          ],                                    
+        ),                                     
+      ),
+    );
+  }
+
+
 }
