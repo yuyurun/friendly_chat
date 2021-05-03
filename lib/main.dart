191e73473f3009c874d73e2403fcf1d17adbc384
yuyurun <yuyu1.run@icloud.com>
2021-05-03 14:35:24 +0900

    feat: display messages

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ void main() {
   );
 }
 
+//認証によって送信者の名前取得できるといいね
+String _name = 'りこ';
+
 class FriendlyChatApp extends StatelessWidget {
   const FriendlyChatApp({
     Key? key,
@@ -20,23 +23,79 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
+class ChatMessage extends StatelessWidget {
+  ChatMessage({required this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class Chatscreen extends StatefulWidget {
   @override
   _ChatscreenState createState() => _ChatscreenState();
 }
 
 class _ChatscreenState extends State<Chatscreen> {
+  final List<ChatMessage> _messages = [];
   final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: text,
+    );
+    setState((){
+      _messages.insert(0, message);
+    });
+    _focusNode.requestFocus();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('チャットだよ')), 
-      body: _buildTextComposer(),
+      body: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+          ),
+        ],
+      ),
     );
   }
 
@@ -53,6 +112,7 @@ class _ChatscreenState extends State<Chatscreen> {
                 onSubmitted: _handleSubmitted,
                 decoration:  InputDecoration.collapsed(
                     hintText: 'Send a message'),
+                focusNode: _focusNode,
               ),
             ),
             Container(
